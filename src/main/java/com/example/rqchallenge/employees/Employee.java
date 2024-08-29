33f35d111cf7539b7c04a43e9e3cd3f539609c16
Elana Katzen <ekatzen@terpmail.umd.edu>
2024-08-29 06:01:13 -0400

    add employee model

---
+++ src/main/java/com/example/rqchallenge/employees/Employee.java
@@ -0,0 +1,19 @@
+package com.example.rqchallenge.employees;
+
+
+public class Employee {
+    private String id;
+
+    private String employeeName;
+
+    private String employeeSalary;
+
+    private String employeeAge;
+
+    private String profileImage;
+
+    public Employee() {
+        
+    }
+
+}
